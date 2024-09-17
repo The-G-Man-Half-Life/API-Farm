c509250f9739bd0c7070b092cade26ea7f9bba1c
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-09-17 08:11:17 -0500

    feat: the controller animal types was configurated

---
+++ Controllers/v1/AnimalTypes/AnimalTypesController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Farm.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Farm.Controllers.v1.AnimalTypes;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class AnimalTypesController : ControllerBase
+{
+    private readonly ApplicationDbContext Context;
+
+    public AnimalTypesController(ApplicationDbContext context)
+    {
+        Context = context;
+    }
+
+}
